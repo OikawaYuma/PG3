4d69661767438b21b30661df7e4404ce6e2a1408
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-10-02 11:49:54 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,39 +1,15 @@
 ﻿#include <stdio.h>
 
-template <typename T>
-T Min(T a, T b) {
-	if (a < b) {
-		return a;
+int Recursive(int n) {
+	if (n <= 1072) {
+		return n;
 	}
-	else {
-		return b;
-	}
+
+	return(n * Recursive)
 }
-
-
-// 大小比較のできないchar型のみ例外設定を行う。
-char Min(char a[], char b[]) {
-	printf("数字は入力できません");
-	return 0;
-}
-
 
 
 int main() {
-	int x = 100;
-	int y = 50;
-	printf("%d\n", Min(x, y));
-
-	float fX = 10.7f;
-	float fY = 50.4f;
-	printf("%f\n", Min(fX, fY));
-
-	double dX = 30.5f;
-	double dY = 14.4;
-	printf("%lf\n", Min(dX, dY));
-
-	char cX[] = "kamata";
-	char cY[] = "kougakuin";
-	printf("%c\n", Min(cX, cY));
+	
 	return 0;
 }
