e0f8004d11ec976013432c31a4545fcf3011897b
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-10-16 10:59:55 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ int maney = 100;
 int sum = 0;
 int Recursive(int n) {
 	sum += maney;
-	printf("%d時間目,一般的賃金%d , 変動時給%d :合計賃金%d\n", num, num * 1072, maney, sum);
+	printf("%d時間目 , 一般的賃金 : %d , 変動時給 : %d , 合計賃金 : %d\n", num, num * 1072, maney, sum);
 	if (sum >= 1072*num) {
-		printf("%d\n", num);
-		printf("%d\n", num*1072);
-		printf("%d", sum);
+		printf("\n超えるための時間 %d\n", num);
+		printf("一般の賃金 %d\n", num*1072);
+		printf("変動の賃金 %d", sum);
 		return sum;
 	}
 	
