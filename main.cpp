82384187f6fb642f14ecb6cbeafdf89f4ac70e35
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-10-16 12:04:29 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
 ﻿#include <stdio.h>
 
-template <typename T>
-T Min(T a, T b) {
+int Min(int a, int b) {
 	if (a < b) {
-		return a;
+		printf("%d\n", a);
+		return 0;
 	}
 	else {
-		return b;
+		printf("%d\n,", b);
+		return 0;
+	}
+}
+float Min(float a, float b) {
+	if (a < b) {
+		printf("%f\n", a);
+		return 0;
+	}
+	else {
+		printf("%f\n", b);
+		return 0;
+	}
+}
+double Min(double a, double b) {
+	if (a < b) {
+		printf("%lf\n", a);
+		return 0;
+	}
+	else {
+		printf("%lf\n", b);
+		return 0;
 	}
 }
 
 
+
 // 大小比較のできないchar型のみ例外設定を行う。
-char Min(char a[], char b[]) {
+ char Min(char *a, char *b) {
 	printf("数字は入力できません");
 	return 0;
 }
@@ -22,15 +44,15 @@ char Min(char a[], char b[]) {
 int main() {
 	int x = 100;
 	int y = 50;
-	printf("%d\n", Min(x, y));
+	Min(x, y);
 
 	float fX = 10.7f;
 	float fY = 50.4f;
-	printf("%f\n", Min(fX, fY));
+	 Min(fX, fY);
 
 	double dX = 30.5f;
 	double dY = 14.4;
-	printf("%lf\n", Min(dX, dY));
+	Min(dX, dY);
 
 	char cX[] = "kamata";
 	char cY[] = "kougakuin";
