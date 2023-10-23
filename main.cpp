2ca103fa98b836135dbd24e4aeb02b94ee6692c0
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-10-23 10:23:53 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -1,61 +1,33 @@
-﻿#include <stdio.h>
-
-int Min(int a, int b) {
-	if (a < b) {
-		printf("%d\n", a);
-		return 0;
-	}
-	else {
-		printf("%d\,", b);
-		return 0;
-	}
-}
-float Min(float a, float b) {
-	if (a < b) {
-		printf("%f\n", a);
-		return 0;
-	}
-	else {
-		printf("%f\n", b);
-		return 0;
-	}
-}
-double Min(double a, double b) {
-	if (a < b) {
-		printf("%lf\n", a);
-		return 0;
-	}
-	else {
-		printf("%lf\n", b);
-		return 0;
+﻿#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include <math.h>
+void TimeCount() {
+	int countTime = 180;
+	while (countTime <= 0) {
+		countTime--;
 	}
 }
 
+int RandNum() {
+	unsigned int currentTime = time(nullptr);
+	srand(currentTime);
+	int b = rand() % 6+1;
 
-
-// 大小比較のできないchar型のみ例外設定を行う。
- char Min(char *a, char *b) {
-	printf("数字は入力できません");
-	return 0;
+	return b;
 }
 
+void SelectNum() {
 
+}
 
 int main() {
-	int x = 100;
-	int y = 50;
-	Min(x, y);
-
-	float fX = 10.7f;
-	float fY = 50.4f;
-	 Min(fX, fY);
-
-	double dX = 30.5f;
-	double dY = 14.4;
-	Min(dX, dY);
-
-	char cX[] = "kamata";
-	char cY[] = "kougakuin";
-	Min(cX, cY);
+	
+	int (*pfunc)();
+	pfunc = RandNum;
+	int num = pfunc();
+	int a = 0;
+	scanf_s("%d",a);
+	
 	return 0;
 }
