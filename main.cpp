5ab98287110d898c35f07cf0cb48eae4615e23e7
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-10-23 10:54:34 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -2,32 +2,3 @@
 #include<stdio.h>
 #include<time.h>
 #include <math.h>
-void TimeCount() {
-	int countTime = 180;
-	while (countTime <= 0) {
-		countTime--;
-	}
-}
-
-int RandNum() {
-	unsigned int currentTime = time(nullptr);
-	srand(currentTime);
-	int b = rand() % 6+1;
-
-	return b;
-}
-
-void SelectNum() {
-
-}
-
-int main() {
-	
-	int (*pfunc)();
-	pfunc = RandNum;
-	int num = pfunc();
-	int a = 0;
-	scanf_s("%d",a);
-	
-	return 0;
-}
