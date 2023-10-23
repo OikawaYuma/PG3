d2b166e274b56e1a311f938da0c4a29e8033c01f
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-10-23 11:26:06 +0900

    完成part2

--- main.cpp
+++ main.cpp
@@ -1,61 +1,40 @@
 ﻿#include <stdio.h>
+template <typename Type>
+Type Min(Type a, Type b) {
+	if (a < b) {
+		
+		return static_cast<Type>(a);
+	}
+	else {
+		return static_cast<Type>(b);
+	}
 
-int Min(int a, int b) {
-	if (a < b) {
-		printf("%d\n", a);
-		return 0;
-	}
-	else {
-		printf("%d\,", b);
-		return 0;
-	}
-}
-float Min(float a, float b) {
-	if (a < b) {
-		printf("%f\n", a);
-		return 0;
-	}
-	else {
-		printf("%f\n", b);
-		return 0;
-	}
-}
-double Min(double a, double b) {
-	if (a < b) {
-		printf("%lf\n", a);
-		return 0;
-	}
-	else {
-		printf("%lf\n", b);
-		return 0;
-	}
+	
 }
 
 
-
-// 大小比較のできないchar型のみ例外設定を行う。
  char Min(char *a, char *b) {
-	printf("数字は入力できません");
+	 printf("数字は入力できません");
 	return 0;
 }
 
 
 
 int main() {
-	int x = 100;
-	int y = 50;
-	Min(x, y);
+	int x = 114;
+	int y = 514;
+	printf("%d\n",Min<int>(x, y));
 
-	float fX = 10.7f;
-	float fY = 50.4f;
-	 Min(fX, fY);
+	float fx = 10.7f;
+	float fy = 56.4f;
+	printf("%f\n", Min<float>(fx, fy));
+	
+	double dx = 1033.743f;
+	double dy = 526.456f;
+	printf("%lf\n", Min<float>(dx, dy));
 
-	double dX = 30.5f;
-	double dY = 14.4;
-	Min(dX, dY);
-
-	char cX[] = "kamata";
-	char cY[] = "kougakuin";
-	Min(cX, cY);
+	char cx[] = "kamata";
+	char cy[] = "kogakuin";
+	Min(cx, cy);
 	return 0;
 }
