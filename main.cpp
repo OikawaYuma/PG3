8663c35410724b7379cc2fa17e8c7f1d98a41b7a
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-11-06 11:46:30 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,41 +1,16 @@
-﻿#include<stdlib.h>
+#include<stdlib.h>
 #include<stdio.h>
-#include<time.h>
-#include <math.h>
 
-typedef void (*PFunc)(int*);
-void TimeCount() {
-	int countTime = 180;
-	while (countTime <= 0) {
-		countTime--;
-	}
-}
+#include <iostream>
+#include <Windows.h>
 
-int RandNum() {
-	unsigned int currentTime = time(nullptr);
-	srand(currentTime);
-	int b = rand() % 6+1;
 
-	return b;
-}
-
-int SelectNum(int num) {
-	return 0;
-}
-
-void SetTimeOut(PFunc p,int second) {
-
-}
 
 int main() {
+	SetConsoleOutputCP(65001);
+	char str[] = "ア";
+	printf("%s", str);
 	
-	int (*pfunc)();
-	pfunc = RandNum;
-	int num = pfunc();
-
-	pfunc = SelectNum;
-	int a = 0;
-	scanf_s("%d",&a);
 	
 	return 0;
 }
