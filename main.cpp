f3d774b443e6230251c6f82ebc4283e4fd43f6fd
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-11-07 23:39:09 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -13,19 +13,22 @@ auto RandNum = [](int i) {
 
 };
 // 渡された数字が偶数か、奇数を判断する
-auto SetTimeOutAnswer = [](int randNum) {
+std::function<void(int)> SetTimeOutAnswer = [](int randNum) {
 	//サイコロの目を奇数か、偶数かを判断する
 	int judgeNum = randNum % 2;
 	//プレイヤーの予想を代入する
 	int answerNum;
-	scanf_s("サイコロの目が偶数なら0、奇数ならそれ以外の数字を入力してください : %d\n", &answerNum);
+	printf("サイコロの目が偶数なら0、奇数ならそれ以外の数字を入力してください : ");
+	scanf_s("%d",&answerNum);
 	Sleep(3 * 1000);
 	if ((judgeNum == 0 && answerNum == 0) || (judgeNum == 1 && answerNum != 0)) {
-		printf("正解");
+		printf("\n正解");
 	}
-	else { printf("不正解"); };
+	else { printf("\n不正解"); };
 
-	printf("\n%d", randNum);
+	printf("\nプレイヤーの選んだ数字　%d", answerNum);
+	printf("\n偶数か奇数か　%d", judgeNum);
+	printf("\nサイコロの目　%d", randNum);
 };
 
 int main() {
