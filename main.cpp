874265ee78b73bc8deb5bb154713609cdac36e83
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-11-07 21:36:53 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -3,33 +3,24 @@
 #include<time.h>
 #include <math.h>
 #include <Windows.h>
+#include <functional>
 
-typedef void (*PFunc)(int);
-void TimeCount() {
-	int countTime = 180;
-	while (countTime <= 0) {
-		countTime--;
-	}
-}
-
-int RandNum() {
+auto num = [](int i) {
 	unsigned int currentTime = time(nullptr);
 	srand(currentTime);
-	int b = rand() % 6 + 1;
+	i = rand() % 6 + 1;
+	return i;
 
-	return b;
-}
+};
 
-int SelectNum(int num) {
-	return 0;
-}
+//void SetTimeOutAnswer( PFunc Answer,int randNum) {
+//	
+//	Answer(randNum);
+//
+//}
 
-void SetTimeOutAnswer( PFunc Answer,int randNum) {
-	
-	Answer(randNum);
 
-}
-void Answer(int randNum) {
+auto Answer = [](int randNum) {
 	//サイコロの目を奇数か、偶数かを判断する
 	int judgeNum = randNum % 2;
 	//プレイヤーの予想を代入する
@@ -40,19 +31,20 @@ void Answer(int randNum) {
 		printf("正解");
 	}
 	else { printf("不正解"); };
-}
+};
 
 int main() {
 	//サイコロを転がす処理
-	int (*pfunc)();
-	pfunc = RandNum;
-	int num = pfunc();
+
+	num(1);
+
+	printf("%d", num);
 
 	// 奇数か偶数か選択し、正否を返す
-	PFunc pAnswer;
-	pAnswer = Answer;
 
-	SetTimeOutAnswer( Answer,num);
+	Answer(num(2));
+
+	/*SetTimeOutAnswer( Answer,num);*/
 	
 	// サイコロの目の数を確認
 	printf("\n%d", num);
