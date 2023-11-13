a8c088260b81f317be3bc7885a8fe43096024895
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-11-13 12:25:53 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -14,27 +14,27 @@ int main() {
 	Number<double,double>* DoubleNum = new Number<double,double>(71.6, 45.6);
 
 
-	 int Answer = Number<int,int>::Min(IntNum->number1, IntNum->number2);
+	 int AnswerII = Number<int,int>::Min(IntNum->number1, IntNum->number2);
 	 float AnswerIF = Number<int, float>::Min(IntNum->number1, FloatNum->number2);
 	 double AnswerID = Number<int, double>::Min(IntNum->number1, DoubleNum->number2);
 
 
 
-	 float Answerf = Number<float,float>::Min(FloatNum->number1, FloatNum->number2);
-	 double AnswerfD = Number<float, float>::Min(FloatNum->number1, DoubleNum->number2);
+	 float AnswerFF = Number<float,float>::Min(FloatNum->number1, FloatNum->number2);
+	 double AnswerFD = Number<float, float>::Min(FloatNum->number1, DoubleNum->number2);
 
-	 double Answerd = Number<double,double>::Min(DoubleNum->number1, DoubleNum->number2);
-
-
+	 double AnswerDD = Number<double,double>::Min(DoubleNum->number1, DoubleNum->number2);
 
 
-	printf("%d\n", Answer);
+
+
+	printf("%d\n", AnswerII);
 	printf("%f\n", AnswerIF);
 	printf("%lf\n", AnswerID);
 
-	printf("%f\n", Answerf);
-	printf("%lf\n", AnswerfD);
+	printf("%f\n", AnswerFF);
+	printf("%lf\n", AnswerFD);
 
-	printf("%lf\n", Answerd);
+	printf("%lf\n", AnswerDD);
 	return 0;
 }
