904a447cafa12653d2cf7ed02a5b30872f750e71
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-11-20 12:57:49 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <Windows.h>
 #include <functional>
 
-typedef  void(*PFunc)(int,int);
+typedef  std::function<void()>(*PFunc)();
 
 
 
@@ -20,6 +20,7 @@ auto RandNum = []() {
 };
 
 auto SelectNum = []() {
+	printf("サイコロの目が偶数なら0、奇数ならそれ以外の数字を入力してください : ");
 	int selectNum;
 	scanf_s("%d", &selectNum);
 	return selectNum;
@@ -27,34 +28,65 @@ auto SelectNum = []() {
 
 
 // 渡された数字が偶数か、奇数を判断する
-void SetTimeOut(PFunc answer,int setTime) {
+void SetTimeOut(std::function<void()> p, int setTime) {
 	Sleep(setTime * 1000);
-	answer(RandNum(),SelectNum());
+	p();
 };
-	
-void Answer ( int randNum, int selectNum){
 
-	//サイコロの目を奇数か、偶数かを判断する
-	int judgeNum = randNum % 2;
-	//プレイヤーの予想を代入する
-	int answerNum =selectNum;
+std::function<int()> Judge = []() {return 0; };
 
-	if ((judgeNum == 0 && answerNum == 0) || (judgeNum == 1 && answerNum != 0)) {
+std::function<void()> Answer = []() {
+	int judge = Judge();
+	printf("\n 結果　%d", judge);
+	if (judge) {
 		printf("\n正解");
 	}
-	else { printf("\n不正解"); };
-
-	printf("\nプレイヤーの選んだ数字　%d", answerNum);
-	printf("\n偶数か奇数か　%d", judgeNum);
-	printf("\nサイコロの目　%d", randNum);
+	else if (!judge) {
+		printf("\n不正解");
+	}
 };
-
 
 
 int main() {
-	printf("サイコロの目が偶数なら0、奇数ならそれ以外の数字を入力してください : ");
-	PFunc answer = Answer;
+	int randNum = RandNum();
+	int selectNum = SelectNum();
+
+	auto Judge = [&]() {
+		//サイコロの目を奇数か、偶数かを判断する
+		int judgeNum = randNum % 2;
+		//プレイヤーの予想を代入する
+		int answerNum = selectNum;
+		if ((judgeNum == 0 && answerNum == 0) || (judgeNum == 1 && answerNum != 0)) {
+
+			return 1;
+		}
+		else {
+			return 0;
+		};
+
+	};
+
+	std::function<void()> Answer = [&]() {
+		int judge = Judge();
+		printf("\n 結果　%d", judge);
+		if (judge) {
+			printf("\n正解");
+		}
+		else if (!judge) {
+			printf("\n不正解");
+		}
+	};
+
+
+
+
+	std::function<void()>  answer = Answer;
 	SetTimeOut(answer, 3);
 
+
+	// 確認
+	printf("\nプレイヤーの選んだ数字　%d", selectNum);
+	printf("\n偶数か奇数か　%d", selectNum%2);
+	printf("\nサイコロの目　%d", randNum);
 	return 0;
 }
