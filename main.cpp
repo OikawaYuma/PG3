5e49d79db1d1d95f74cc6d574ef50acd5e4cee86
OikawaYuma <k022g0016@g.neec.ac.jp>
2024-02-08 18:05:49 +0900

    提出

--- main.cpp
+++ main.cpp
@@ -12,18 +12,18 @@
 
 
 int main() {
-	std::string a(100000, 'a');
-	std::string b;
-	std::string c;
+	std::string testChar(100000, 'a');
+	std::string coppyChar;
+	std::string moveChar;
 	std::chrono::system_clock::time_point start, end;
 	start = std::chrono::system_clock::now();
-	b = a;
+	coppyChar = testChar;
 	end = std::chrono::system_clock::now();
 	double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 	std::cout << elapsed << std::endl;
 	std::chrono::system_clock::time_point start2, end2;
 	start = std::chrono::system_clock::now();
-	c = std::move(a);
+	moveChar = std::move(testChar);
 	end = std::chrono::system_clock::now();
 	double elapsed2 = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 	std::cout << elapsed2<< std::endl;
